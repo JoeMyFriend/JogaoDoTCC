c3acbc9d3ecb0a8829927fac04671c7a45cde164
JoeMyFriend <joao.paulo.miquilini@gmail.com>
2016-12-04 14:30:36 -0200

    Material e Script de BackGround

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour {
+
+	private Transform cameraVar;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		cameraVar = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 novaPosicao = new Vector3 (cameraVar.position.x, transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, novaPosicao, Time.time);
+	}
+}
